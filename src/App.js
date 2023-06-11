e97a1f3873879d5d1369a1fd0473f47a369f9049
mostafa elkabany <mostafa@ardentpeak.com>
2023-06-11 23:33:38 +0300

    single example display

--- src/App.js
+++ src/App.js
@@ -7,64 +7,71 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   const dispatch = useDispatch();
-  
-  const mockFetchRules = () => [
-    { 
-      id: 1, 
-      topic: 'Noun Case Endings',
-      rule: 'In Arabic, nouns can have different case endings...',
-      highlightColor: '#FF0000', 
-      sound: 'https://tmpfiles.org/dl/1533068/alt-jbreezeblocks.mp3',
-      examples: [
-        {
-          sentence: 'This is a test sentence.',
-          highlightedIndices: [0, 1, 2]
-        },
-        {
-          sentence: 'This is another test sentence.',
-          highlightedIndices: [5, 6, 7]
-        }
-      ]
-    },
-    { 
-      id: 2, 
-      topic: 'Verb Conjugation',
-      rule: 'Arabic verbs change based on the subject...',
-      highlightColor: '#00FF00', 
-      sound: 'https://tmpfiles.org/dl/1533068/alt-jbreezeblocks.mp3', 
-      examples: [
-        {
-          sentence: 'This is a test sentence for verb conjugation.',
-          highlightedIndices: [0, 1, 2]
-        },
-        {
-          sentence: 'This is another test sentence for verb conjugation.',
-          highlightedIndices: [5, 6, 7]
-        }
-      ]
-    },
-    { 
-      id: 3, 
-      topic: 'Adjective Agreement',
-      rule: 'Adjectives in Arabic agree with the noun they modify...',
-      highlightColor: '#0000FF', 
-      sound: 'https://tmpfiles.org/dl/1533068/alt-jbreezeblocks.mp3', 
-      examples: [
-        {
-          sentence: 'This is a test sentence for adjective agreement.',
-          highlightedIndices: [0, 1, 2]
-        },
-        {
-          sentence: 'This is another test sentence for adjective agreement.',
-          highlightedIndices: [5, 6, 7]
-        }
-      ]
-    }
-  ];
+
+  const mockFetchRules = () => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve([
+          { 
+            id: 1, 
+            topic: 'Noun Case Endings',
+            rule: 'In Arabic, nouns can have different case endings...',
+            highlightColor: '#FF0000', 
+            sound: 'https://tmpfiles.org/dl/1533068/alt-jbreezeblocks.mp3',
+            examples: [
+              {
+                sentence: 'This is a test sentence.',
+                highlightedIndices: [0, 1, 2]
+              },
+              {
+                sentence: 'This is another test sentence.',
+                highlightedIndices: [5, 6, 7]
+              }
+            ]
+          },
+          { 
+            id: 2, 
+            topic: 'Verb Conjugation',
+            rule: 'Arabic verbs change based on the subject...',
+            highlightColor: '#00FF00', 
+            sound: 'https://tmpfiles.org/dl/1533068/alt-jbreezeblocks.mp3', 
+            examples: [
+              {
+                sentence: 'This is a test sentence for verb conjugation.',
+                highlightedIndices: [0, 1, 2]
+              },
+              {
+                sentence: 'This is another test sentence for verb conjugation.',
+                highlightedIndices: [5, 6, 7]
+              }
+            ]
+          },
+          { 
+            id: 3, 
+            topic: 'Adjective Agreement',
+            rule: 'Adjectives in Arabic agree with the noun they modify...',
+            highlightColor: '#0000FF', 
+            sound: 'https://tmpfiles.org/dl/1533068/alt-jbreezeblocks.mp3', 
+            examples: [
+              {
+                sentence: 'This is a test sentence for adjective agreement.',
+                highlightedIndices: [0, 1, 2]
+              },
+              {
+                sentence: 'This is another test sentence for adjective agreement.',
+                highlightedIndices: [5, 6, 7]
+              }
+            ]
+          }
+        ]);
+      }, 200);
+    });
+  };
 
   useEffect(() => {
-    const rules = mockFetchRules();
-    dispatch(setRules(rules));
+    mockFetchRules().then((rules) => {
+      dispatch(setRules(rules));
+    });
   }, [dispatch]);
 
   const rules = useSelector(state => state.rule);
--- src/RuleDisplay.js
+++ src/RuleDisplay.js
@@ -1,4 +1,5 @@
-import React from 'react';
+// RuleDisplay.js
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import SentenceDisplay from './SentenceDisplay';
@@ -7,6 +8,7 @@ function RuleDisplay() {
   const { topicId } = useParams();
   const rules = useSelector(state => state.rule);
   const rule = rules.find(rule => rule.id === parseInt(topicId));
+  const [currentExampleIndex, setCurrentExampleIndex] = useState(0);  // new state
 
   // If the rule is not found, display a loading message
   if (!rule) return <div>Loading...</div>;
@@ -14,15 +16,26 @@ function RuleDisplay() {
   // Make sure that the rule has an examples array before trying to map over it
   if (!rule.examples) return <div>No examples found for this rule.</div>;
 
+  const handleNextExample = () => {
+    if (currentExampleIndex < rule.examples.length - 1) {
+      setCurrentExampleIndex(currentExampleIndex + 1);
+    }
+  };
+
+  const handlePrevExample = () => {
+    if (currentExampleIndex > 0) {
+      setCurrentExampleIndex(currentExampleIndex - 1);
+    }
+  };
+
   return (
     <div>
       <h1>{rule.topic}</h1>
-      {rule.examples.map((example, index) => (
-        <SentenceDisplay key={index} example={example} rule={rule} />
-      ))}
+      <SentenceDisplay example={rule.examples[currentExampleIndex]} rule={rule} />
+      <button onClick={handlePrevExample}>Prev</button>
+      <button onClick={handleNextExample}>Next</button>
     </div>
   );
 }
-
 
 export default RuleDisplay;
